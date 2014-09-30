a1aa7664e6e03af9094473ee830b9ea800456e40
Vicecalidadumb <yeison.arias@umb.edu.co>
2014-09-30 11:37:40 -0500

    Ultimos Cambios
    Ultimos Cambios

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="shortcut icon" href="<?php echo base_url('images/vice/favicon.png'); ?>">
+        <link rel="shortcut icon" href="<?php echo base_url('images/favicon.ico'); ?>">
 
         <title><?php echo $title; ?></title>
 
